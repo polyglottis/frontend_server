65876482391f6dfa76b6e61fcdfc6d7e61cad150
oliglottis <olivier@polyglottis.org>
2014-08-13 17:39:49 +0200

    Use template root from config file; basic style

--- templates/parse.go
+++ templates/parse.go
@@ -2,15 +2,19 @@ package templates
 
 import (
 	"html/template"
+
+	"github.com/polyglottis/platform/config"
 )
 
 func Parse(files ...string) *template.Template {
 	// always include base template
 	files = append([]string{"templates/base"}, files...)
 
+	root := config.Get().TemplateRoot
+
 	full := make([]string, len(files))
 	for i, f := range files {
-		full[i] = "github.com/polyglottis/frontend_server/" + f + ".html"
+		full[i] = root + "/" + f + ".html"
 	}
 	return template.Must(template.ParseFiles(full...))
 }
