e940cade0d2a382714e8f6ed6bdd5acd59ddef0e
oliglottis <olivier@polyglottis.org>
2014-08-25 12:03:07 +0200

    Language select looks roughly ok

--- extract/templates/language_select.js
+++ extract/templates/language_select.js
@@ -1,5 +1,5 @@
 {{define "angular-script"}}
-function LanguageCtrl($scope) {
+function LanguageCtrl($scope, $window) {
 	$scope.languages = {{.Data.LanguageOptions}};
 	$scope.changeA = function(lang) {
 		$scope.textA = lang.Text[0];
@@ -13,5 +13,17 @@ function LanguageCtrl($scope) {
 	$scope.languageA = $scope.languages[{{.Data.Selection.LanguageA}}];
 	$scope.textA = $scope.languageA.Text[{{.Data.Selection.TextA}}];
 	{{end}}
+	{{if ne .Data.Selection.LanguageB -1}}
+	$scope.languageB = $scope.languages[{{.Data.Selection.LanguageB}}];
+	$scope.textB = $scope.languageB.Text[{{.Data.Selection.TextB}}];
+	{{end}}
+	var slug = {{.Slug}};
+	$scope.submit = function(args) {
+		var loc = "/extract/" + slug + "/" + $scope.languageA.Code + "?ta=" + $scope.textA.Id;
+		if ($scope.languageB) {
+			loc += "&b=" + $scope.languageB.Code + "&tb=" + $scope.textB.Id;
+		}
+		$window.location = loc;
+	};
 }
 {{end}}
